9a251d88d9d355fada4cea46db851b5adcdefd9c
Heng <alivetaxi@gmail.com>
2021-10-30 12:32:22 +0800

    dashboards

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.contrib import messages, auth
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 
+from contacts.models import Contact
+
 
 def register(request):
     if request.method == 'POST':
@@ -58,4 +60,8 @@ def logout(request):
 
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_contacts = Contact.objects.all().order_by('-contact_date').filter(user_id=request.user.id)
+    context = {
+        'contacts': user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context)
